c8b83a2454bd176baccaf5b5912a936068d86f7e
sumanthdevopsgit <93962528+sumanthdevopsgit@users.noreply.github.com>
2023-10-07 10:19:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIATNE64FXHHQVJSZTW"
+  secret_key = "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"
 
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "kljklz"
+  bucket = "s3cfdist"
   acl    = "private"
 
   versioning {
